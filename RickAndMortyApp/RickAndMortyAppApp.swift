d5e2972cfdf504991264e2f13a0b292b0b11fd5d
nerzul88 <nerzul.88@gmail.com>
2023-01-17 19:44:19 +0300

    Initial commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Александр Касьянов on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
